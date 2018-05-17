e713527c4632fdbb58fc919d1c40944b90827877
Yeramian Kevin <kevin.yeramian@eyesnap.fr>
2018-05-17 11:53:18 +0200

    Feat: Update of the score function

--- Engine/Searcher.java
+++ Engine/Searcher.java
@@ -9,6 +9,7 @@ package Engine;
 import java.util.ArrayList;
 import java.util.*;
 import Engine.Query.QueryTerm;
+import scala.annotation.meta.param;
 
 
 public class Searcher {
@@ -29,7 +30,7 @@ public class Searcher {
      *  Searches the index for postings matching the query.
      *  @return A postings list representing the result of the query.
      */
-    public ArrayList<QuestionAnswer> search( Query query, QueryType queryType, RankingType rankingType, ElasticSearchInterface esi ) {
+    public ArrayList<Pair> search( Query query, QueryType queryType, RankingType rankingType, ElasticSearchInterface esi ) {
     	   if(esi != null) {
 	    	   Pair pair;
 	    	   ArrayList<Pair> synonymList = new ArrayList<Pair>();
@@ -54,39 +55,126 @@ public class Searcher {
     	return kGramTerm;
     }
     
-    public ArrayList<QuestionAnswer> score(ArrayList<QuestionAnswer> qaList, Query query) {
+    public ArrayList<Pair> score(ArrayList<QuestionAnswer> qaList, Query query) {
+    	if(qaList == null) {
+    		return new ArrayList<Pair>();
+    	}
+    	HashMap<String,Integer> uniqueId = new HashMap<String,Integer>();
+    	HashMap<String,Integer> string2N = new HashMap<String,Integer>();
+    	HashMap<String,Integer> string2NQuery = new HashMap<String,Integer>();
+    	HashMap<String,Integer> string2NQuestion = new HashMap<String,Integer>();
     	
-    	
+    	int currentID = 0;
     	ArrayList<Pair> queryKgram = new ArrayList<Pair>();
     	ArrayList<String> queryString = new ArrayList<String>();
-    	ArrayList<QuestionAnswer> output = new ArrayList<QuestionAnswer>();
+    	ArrayList<Pair> output = new ArrayList<Pair>();
     	ArrayList<Pair> answerKgram = new ArrayList<Pair>();
     	ArrayList<String> answerString = new ArrayList<String>();
     	
     	for(QueryTerm queryterm : query.queryterm) {
     		ArrayList<String> kGramTerm = kGram(queryterm.term);
+    		string2N.clear();
     		for(String kgram : kGramTerm) {
     			if(!queryString.contains(kgram)) {
     				queryKgram.add(new Pair(kgram,queryterm.weight));
     				queryString.add(kgram);
+    				Integer value = string2N.get(kgram);
+    				if(value == null) {
+    					string2N.put(kgram, 1);
+    				}
+    				
+    			}
+    			else {
+    				int index = queryString.indexOf(kgram);
+    				queryKgram.get(index).value = Math.max(queryKgram.get(index).value, queryterm.weight);
+    				Integer value = string2N.get(kgram);
+    				if(value == null) {
+    					string2N.put(kgram, 1);
+    				}
+    			}
+    		}
+    		
+    		for(String key : string2N.keySet()) {
+    			Integer value = string2NQuery.get(key);
+    			if(value == null) {
+    				string2NQuery.put(key, 1);
+    			}
+    			else {
+    				string2NQuery.put(key, 1 + value);
     			}
     		}
  	   	}
+    	
     	for(QuestionAnswer qa : qaList) {
-    		answerKgram.clear();
-    		answerString.clear();
+    		
+    		
     		for(SearchEntry se: qa.answer) {
-    			ArrayList<String> kGramTerm = kGram(se.text);
-    			for(String kgram : kGramTerm) {
-        			if(!queryString.contains(kgram)) {
-        				answerKgram.add(new Pair(kgram,se.rank));
-        				answerString.add(kgram);
-        			}
+    			answerKgram.clear();
+        		answerString.clear();
+        		string2NQuestion.clear();
+    			for(String term : se.text.split(" ")) {
+    				
+    				ArrayList<String> kGramTerm = kGram(term);
+    				string2N.clear();
+        			for(String kgram : kGramTerm) {
+            			if(!answerString.contains(kgram)) {
+            				answerKgram.add(new Pair(kgram,se.rank));
+            				answerString.add(kgram);
+            				Integer value = string2N.get(kgram);
+            				if(value == null) {
+            					string2N.put(kgram, 1);
+            				}
+            			}
+            			else {
+            				int index = answerString.indexOf(kgram);
+            				answerKgram.get(index).value = Math.max(answerKgram.get(index).value, se.rank);
+            				Integer value = string2N.get(kgram);
+            				if(value == null) {
+            					string2N.put(kgram, 1);
+            				}
+            			}
+            		}
+        			for(String key : string2N.keySet()) {
+            			Integer value = string2NQuery.get(key);
+            			if(value == null) {
+            				string2NQuestion.put(key, 1);
+            			}
+            			else {
+            				string2NQuestion.put(key, 1 + value);
+            			}
+            		}
+        			
+    			}
+    			// End of term in text
+    			
+    			
+    			double questionScore = 0.0;
+        		for(Pair pair : answerKgram) {
+        			int index = queryString.indexOf(pair.key);
+            		
+            		if(index != -1) {
+            			System.out.println(string2NQuery.get(pair.key) + " " + queryKgram.get(index).value + " " + string2NQuestion.get(pair.key) + " " + pair.value);
+            			questionScore += string2NQuery.get(pair.key) *  queryKgram.get(index).value;
+            			questionScore += string2NQuestion.get(pair.key) * pair.value;
+            		}
+            		
+            	}
+        		double divised = 1.0;
+        		/*for(Integer n : string2NQuery.values()) {
+        			divised  += n;
         		}
+        		for(Integer n : string2NQuestion.values()) {
+        			divised  += n;
+        		}*/
+        		output.add(new Pair(se.text, questionScore / divised));
     		}
+    		
+    		
     	}
     	
     	
+    	
+    	output.sort(Comparator.comparing(p -> -p.value));
 		return output;
     	
     }
